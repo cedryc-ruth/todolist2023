1af1261a5073add1f583bda894ef45c8f9a710ce
Bob Sull <bob@sull.com>
2023-03-23 12:20:46 +0100

    Version 0.1 fonctionnelle

--- js/app.js
+++ js/app.js
@@ -3,5 +3,23 @@
 const inputTask = document.forms['frmTODO'].task;
 const btAdd = document.forms['frmTODO'].btAdd;
 const taskList = document.getElementById('taskList');
+const divNotification = document.getElementById('notification');
 
 //console.log(inputTask);
+
+//Traitement des actions
+btAdd.onclick = function() {
+	//Lecture des données
+	let task = inputTask.value;
+	
+	//Validation des données
+	if(task!='' && task.length>=3) {
+		divNotification.innerHTML = '';
+		
+		//Traitement des données
+		taskList.innerHTML += '<li>'+task+'</li>';
+	} else {
+		//Notification des erreurs
+		divNotification.innerHTML = 'Veuillez entrer une tâche d\'au moins 3 caractères.';
+	}
+};
