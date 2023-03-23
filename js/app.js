69744a33b22967b4975ce833896ac49d6fa0297e
Bob Sull <bob@sull.com>
2023-03-23 11:56:11 +0100

    Ajout du script JS

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/* JS */
+//Sélection des éléments du DOM HTML
+const inputTask = document.forms['frmTODO'].task;
+const btAdd = document.forms['frmTODO'].btAdd;
+const taskList = document.getElementById('taskList');
+
+//console.log(inputTask);
